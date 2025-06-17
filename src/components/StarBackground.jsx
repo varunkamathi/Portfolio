953b8ae447289ce62148201ddf5a104ad30b311c
varunkamathi <varunkamathi522004@gmail.com>
2025-06-17 21:00:22 +0530

    add theme and star background

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+
+ const StarBackground = () => {
+    const [stars, setStars] = useState([])
+   const [meteors, setMeteors] = useState([])
+
+
+    useEffect(()=>{
+        generateStars();
+        generateMeteors();
+
+        const handleResize = () => {
+            generateStars();
+        };
+
+        window.addEventListener('resize', handleResize)
+
+        return () => window.removeEventListener('resize',handleResize);
+
+    },[]);
+
+
+    const generateStars = () => {
+        const numberOfStars = Math.floor(
+            (window.innerWidth* window.innerHeight) / 10000
+        );
+
+        const newStars = [] ;
+
+        for (let i = 0; i < numberOfStars; i++) {
+            newStars.push({
+                id:i,
+                size: Math.random() * 3 + 1 ,
+                x : Math.random() * 100,
+                y : Math.random() * 100,
+                opacity: Math.random() * 0.5 + 0.5 ,
+                animationDuration : Math.random() * 4 + 2,
+            });
+            
+        }
+        setStars(newStars);
+    };
+
+    const generateMeteors= () => {
+        const numberOfMeteors = 5 
+
+        const newMeteors = [] ;
+
+        for (let i = 0; i < numberOfMeteors; i++) {
+            newMeteors.push({
+                id:i,
+                size: Math.random() * 2 + 1 ,
+                x : Math.random() * 100,
+                y : Math.random() * 20,
+                delay: Math.random() * 15 ,
+                animationDuration : Math.random() * 3 + 3,
+            });
+            
+        }
+        setMeteors(newMeteors);
+    };
+
+
+  return (
+    <div className='fixed inset-1 overflow-hidden pointer-events-none z-0'>
+        {stars.map((star)=>(
+            <div key={star.id} className='star animate-pulse-subtle' style={{
+                width: star.size + "px",  
+                height: star.size + "px",                
+                left: star.x + "%",                
+                top: star.y + "%",                
+                opacity: star.opacity,                
+                animationDuration: star.animationDuration + "s",                
+              
+            }}></div>
+        ))
+
+        }
+        {meteors.map((meteors)=>(
+            <div key={meteors.id} className='meteor animate-meteor' style={{
+                width: meteors.size *50 + "px",  
+                height: meteors.size + "px",                
+                left: meteors.x + "%",                
+                top: meteors.y + "%",                
+                animationDelay: meteors.delay,                
+                animationDuration: meteors.animationDuration + "s",                
+              
+            }}></div>
+        ))
+
+        }
+    </div>
+  )
+}
+export default StarBackground;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ThemeToggle from '../components/ThemeToggle';
+import  StarBackground  from '../components/StarBackground';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+    {/*Theme Toggle*/}
+    <ThemeToggle/>
+
+    {/*Star Background */}
+    <StarBackground/>
+      Home
+    </div>
   )
 }
 
