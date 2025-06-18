524542ff5cd591526ead8522bf3faab95c6574bf
varunkamathi <varunkamathi522004@gmail.com>
2025-06-18 16:13:49 +0530

    create Nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ThemeToggle from '../components/ThemeToggle';
 import  StarBackground  from '../components/StarBackground';
+import NavBar from '../components/NavBar';
 
 const Home = () => {
   return (
@@ -10,7 +11,9 @@ const Home = () => {
 
     {/*Star Background */}
     <StarBackground/>
-      Home
+    {/*Navbar */}
+    <NavBar/>
+
     </div>
   )
 }
