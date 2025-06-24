3cdd762e4ea50938361c8b88021b1c51cd9ab8a5
varunkamathi <varunkamathi522004@gmail.com>
2025-06-24 14:59:48 +0530

    update them

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -31,7 +31,7 @@ const ThemeToggle = () => {
             setIsDarkMode(true)
         }
     }
-  return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full",
+  return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-1 right-5 z-50 p-2 rounded-full",
     "transition-colors duration-300 focus:outline-hidden"
   )}> {isDarkMode ? (
      <Sun className='h-6 w-6 text-yellow-300'/>
